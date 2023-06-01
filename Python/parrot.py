e1fcb0c2b75520b31c9bd16f08255b672391c542
Théo Larue <t.larue@lancey.fr>
2023-06-01 11:22:44 +0200

    super keyword

--- Python/parrot.py
+++ Python/parrot.py
@@ -41,4 +41,4 @@ class Parrot:
 class EuropeanParrot(Parrot):
 
     def __init__(self, number_of_coconuts, voltage, nailed):
-        super.__init__(ParrotType.EUROPEAN, number_of_coconuts, voltage, nailed)
+        super().__init__(ParrotType.EUROPEAN, number_of_coconuts, voltage, nailed)
