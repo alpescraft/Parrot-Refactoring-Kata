14367f7b86d673873e319964ad23279e902540d6
MrMattam <void@void.com>
2023-06-01 11:31:27 +0200

    step-commit

--- Java/src/test/java/parrot/ParrotTest.java
+++ Java/src/test/java/parrot/ParrotTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ParrotTest {
-    
+
     @Test
     public void getSpeedOfEuropeanParrot() {
         Parrot parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
@@ -21,7 +21,7 @@ public class ParrotTest {
 
     @Test
     public void getSpeedOfAfricanParrot_With_Two_Coconuts() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
+        GenericParrot parrot = new AfricanParrot( 2, 0, false);
         assertEquals(0.0, parrot.getSpeed(), 0.0);
     }
 
