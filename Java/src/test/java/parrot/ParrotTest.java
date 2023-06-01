c6539daefefb0d54debe60a9ad435e2bf6dcd8e3
DamienG <granierdamien@gmail.com>
2023-06-01 12:12:45 +0200

    utilisation du constructeur sans type dans getSpeedNorwegianBlueParrot_not_nailed_high_voltage

--- Java/src/test/java/parrot/ParrotTest.java
+++ Java/src/test/java/parrot/ParrotTest.java
@@ -44,7 +44,7 @@ public class ParrotTest {
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
-        NorvegianBlueParrot parrot = new NorvegianBlueParrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false);
+        NorvegianBlueParrot parrot = new NorvegianBlueParrot( 0, 4, false);
         assertEquals(24.0, parrot.getSpeed(), 0.0);
     }
 }
