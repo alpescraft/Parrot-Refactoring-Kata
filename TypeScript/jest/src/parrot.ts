218390c255388573243ed02c2ef25899aecc1ef9
Ando Nary <ando.nary@gmail.com>
2023-06-01 11:40:00 +0200

    parrot type african

--- TypeScript/jest/src/parrot.ts
+++ TypeScript/jest/src/parrot.ts
@@ -45,7 +45,7 @@ export class AfricanParrot extends Parrot {
     constructor( numberOfCoconuts: number,
                  voltage: number,
                  isNailed: boolean) {
-        super(ParrotTypes.AFRICAN, numberOfCoconuts, voltage, isNailed);
+        super(0, numberOfCoconuts, voltage, isNailed);
     }
 
     getSpeed(): number {
