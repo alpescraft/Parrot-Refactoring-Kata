22433bac7f7eb412139d5052114596216090d8b4
DamienG <granierdamien@gmail.com>
2023-06-01 11:46:13 +0200

    nouveau parrot norvegian blue

---
+++ Java/src/main/java/parrot/NorvegianBlueParrot.java
@@ -0,0 +1,8 @@
+package parrot;
+
+public class NorvegianBlueParrot extends Parrot {
+
+    public NorvegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+}
