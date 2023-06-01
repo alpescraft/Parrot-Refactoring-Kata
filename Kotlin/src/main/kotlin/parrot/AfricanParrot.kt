1e6b6a47f7f908c76a3c83a5aec0063a118f0b25
Vincent Huc <vincent.huc@pole-emploi.fr>
2023-06-01 11:57:21 +0200

    Correction classe afican

--- Kotlin/src/main/kotlin/parrot/AfricanParrot.kt
+++ Kotlin/src/main/kotlin/parrot/AfricanParrot.kt
@@ -1,1 +1,4 @@
-class AfricanParrot: Parrot ()
+package parrot
+
+class AfricanParrot(type: ParrotTypeEnum, numberOfCoconuts: Int, voltage: Double, isNailed: Boolean) :
+        Parrot(type, numberOfCoconuts, voltage, isNailed)
