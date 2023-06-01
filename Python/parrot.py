ed5cea2ffeb72dad88b1de94f6d41e9cbe42fb80
Thierry de Pauw <thierry@thinkinglabs.io>
2023-06-01 11:59:31 +0200

    Remove unnecessary constructor parameters for AfricanParrot

--- Python/parrot.py
+++ Python/parrot.py
@@ -38,8 +38,8 @@ class EuropeanParrot(Parrot):
         return super()._base_speed()
 
 class AfricanParrot(Parrot):
-    def __init__(self, number_of_coconuts, voltage, nailed):
-        super().__init__(ParrotType.AFRICAN, number_of_coconuts, voltage, nailed)
+    def __init__(self, number_of_coconuts):
+        super().__init__(ParrotType.AFRICAN, number_of_coconuts, 0, False)
 
     def speed(self):
         return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
