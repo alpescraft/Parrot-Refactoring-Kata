f7a5bd561548739fb779182d4df40eb8aaeacfa1
DamienG <granierdamien@gmail.com>
2023-06-01 12:08:10 +0200

    ajout constructeur sans type

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -5,6 +5,10 @@ public class AfricanParrot extends Parrot{
         super(type, numberOfCoconuts, voltage, isNailed);
     }
 
+    public AfricanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(null, numberOfCoconuts, voltage, isNailed);
+    }
+
     @Override
     public double getSpeed() {
         return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
