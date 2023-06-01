e087fffce6b91638977df4228cc632087fe0a809
tibk <thibault.karrer@gmail.com>
2023-06-01 11:49:38 +0200

    Using test framework

--- Python/test_parrot.py
+++ Python/test_parrot.py
@@ -1,36 +1,37 @@
+import unittest
 from parrot import Parrot, AfricanParrot, EuropeanParrot, NorwegianParrot
 
-
-def test_speedOfEuropeanParrot():
-    parrot = EuropeanParrot(0, 0, False)
-    assert parrot.speed() == 12.0
-
-
-def test_speedOfAfricanParrot_With_One_Coconut():
-    parrot = AfricanParrot(1, 0, False)
-    assert parrot.speed() == 3.0
+class TestParrotCase(unittest.TestCase):
+    def test_speedOfEuropeanParrot(self):
+        parrot = EuropeanParrot(0, 0, False)
+        assert parrot.speed() == 12.0
 
 
-def test_speedOfAfricanParrot_With_Two_Coconuts():
-    parrot = AfricanParrot(2, 0, False)
-    assert parrot.speed() == 0.0
+    def test_speedOfAfricanParrot_With_One_Coconut(self):
+        parrot = AfricanParrot(1, 0, False)
+        assert parrot.speed() == 3.0
 
 
-def test_speedOfAfricanParrot_With_No_Coconuts():
-    parrot = AfricanParrot(0, 0, False)
-    assert parrot.speed() == 12.0
+    def test_speedOfAfricanParrot_With_Two_Coconuts(self):
+        parrot = AfricanParrot(2, 0, False)
+        assert parrot.speed() == 0.0
 
 
-def test_speedNorwegianBlueParrot_nailed():
-    parrot = NorwegianParrot(0, 1.5, True)
-    assert parrot.speed() == 0.0
+    def test_speedOfAfricanParrot_With_No_Coconuts(self):
+        parrot = AfricanParrot(0, 0, False)
+        assert parrot.speed() == 12.0
 
 
-def test_speedNorwegianBlueParrot_not_nailed():
-    parrot = NorwegianParrot(0, 1.5, False)
-    assert parrot.speed() == 18.0
+    def test_speedNorwegianBlueParrot_nailed(self):
+        parrot = NorwegianParrot(0, 1.5, True)
+        assert parrot.speed() == 0.0
 
 
-def test_speedNorwegianBlueParrot_not_nailed_high_voltage():
-    parrot = NorwegianParrot(0, 4, False)
-    assert parrot.speed() == 24.0
+    def test_speedNorwegianBlueParrot_not_nailed(self):
+        parrot = NorwegianParrot(0, 1.5, False)
+        assert parrot.speed() == 18.0
+
+
+    def test_speedNorwegianBlueParrot_not_nailed_high_voltage(self):
+        parrot = NorwegianParrot(0, 4, False)
+        assert parrot.speed() == 24.0
