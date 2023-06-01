39ca87a2b6337c861ec11f1551ed1321440cfb87
Ando Nary <ando.nary@gmail.com>
2023-06-01 11:36:31 +0200

    autonomous african parrot

--- TypeScript/jest/src/parrot.ts
+++ TypeScript/jest/src/parrot.ts
@@ -13,8 +13,6 @@ export class Parrot {
 
     public getSpeed(): number {
         switch (this.parrotType) {
-            case ParrotTypes.AFRICAN:
-                return Math.max(0, this.getBaseSpeed() - this.getLoadFactor() * this.numberOfCoconuts);
             case ParrotTypes.NORWEGIAN_BLUE:
                 return (this.isNailed) ? 0 : this.getBaseSpeedWithVoltage(this.voltage);
         }
