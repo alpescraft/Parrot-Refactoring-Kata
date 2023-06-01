773e84b3aec35944bfeb6046993e343e56dc2e32
DamienG <granierdamien@gmail.com>
2023-06-01 11:48:27 +0200

    Override getSpeed from parent

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -1,8 +1,13 @@
 package parrot;
 
-public class EuropeanParrot extends Parrot{
+public class EuropeanParrot extends Parrot {
 
     public EuropeanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(type, numberOfCoconuts, voltage, isNailed);
+    }
+
+    @Override
+    public double getSpeed() {
+        return getBaseSpeed();
     }
 }
