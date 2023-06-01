4557b025d660a181426701d743a450284afbc71c
DamienG <granierdamien@gmail.com>
2023-06-01 12:00:05 +0200

    norvegian has speed

--- Java/src/main/java/parrot/NorvegianBlueParrot.java
+++ Java/src/main/java/parrot/NorvegianBlueParrot.java
@@ -5,4 +5,9 @@ public class NorvegianBlueParrot extends Parrot {
     public NorvegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(type, numberOfCoconuts, voltage, isNailed);
     }
+
+    @Override
+    public double getSpeed() {
+        return (isNailed) ? 0 : getBaseSpeed(voltage);
+    }
 }
