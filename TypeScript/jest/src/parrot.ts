006e066cc760cd0eed1cf95d6235dbe93ab23e76
Ando Nary <ando.nary@gmail.com>
2023-06-01 11:32:41 +0200

    autonomy european parrot

--- TypeScript/jest/src/parrot.ts
+++ TypeScript/jest/src/parrot.ts
@@ -13,8 +13,6 @@ export class Parrot {
 
     public getSpeed(): number {
         switch (this.parrotType) {
-            case ParrotTypes.EUROPEAN:
-                return this.getBaseSpeed();
             case ParrotTypes.AFRICAN:
                 return Math.max(0, this.getBaseSpeed() - this.getLoadFactor() * this.numberOfCoconuts);
             case ParrotTypes.NORWEGIAN_BLUE:
