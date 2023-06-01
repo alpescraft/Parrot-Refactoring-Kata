2d36c30573d7c0e186a2a0822b9d2106ffbb78e5
Théo Larue <t.larue@lancey.fr>
2023-06-01 11:41:27 +0200

    remove coconuts in parent class

--- Python/parrot.py
+++ Python/parrot.py
@@ -11,13 +11,10 @@ class Parrot:
 
     def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
         self._type = type_of_parrot
-        self._number_of_coconuts = number_of_coconuts
         self._voltage = voltage
         self._nailed = nailed
 
     def speed(self):
-        if self._type == ParrotType.AFRICAN:
-            return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
         if self._type == ParrotType.NORWEGIAN_BLUE:
             if self._nailed:
                 return 0
