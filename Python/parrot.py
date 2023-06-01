415b910e2b217d998ad04e3ec5725e06ed77670d
Théo Larue <t.larue@lancey.fr>
2023-06-01 11:52:38 +0200

    generic parrot does not has voltage

--- Python/parrot.py
+++ Python/parrot.py
@@ -11,7 +11,6 @@ class Parrot:
 
     def __init__(self, type_of_parrot, voltage, nailed):
         self._type = type_of_parrot
-        self._voltage = voltage
         self._nailed = nailed
 
     def speed(self):
