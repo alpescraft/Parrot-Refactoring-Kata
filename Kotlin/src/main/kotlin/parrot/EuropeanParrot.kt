76c06ff30bc97be6d9b3d58d4b051b107ed391ae
Vincent Huc <vincent.huc@pole-emploi.fr>
2023-06-01 11:57:33 +0200

    Correction classe eureopean

--- Kotlin/src/main/kotlin/parrot/EuropeanParrot.kt
+++ Kotlin/src/main/kotlin/parrot/EuropeanParrot.kt
@@ -1,1 +1,4 @@
-class EuropeanParrot: Parrot ()
+package parrot
+
+class EureopeanBlueParrot(type: ParrotTypeEnum, numberOfCoconuts: Int, voltage: Double, isNailed: Boolean) :
+        Parrot(type, numberOfCoconuts, voltage, isNailed)
