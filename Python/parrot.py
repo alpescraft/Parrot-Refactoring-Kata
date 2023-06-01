f5748ca6f215a16e78bdba4df263d37f3113a44a
Théo Larue <t.larue@lancey.fr>
2023-06-01 11:25:02 +0200

    african parrottype

--- Python/parrot.py
+++ Python/parrot.py
@@ -47,4 +47,4 @@ class EuropeanParrot(Parrot):
 class AfricanParrot(Parrot):
 
     def __init__(self, number_of_coconuts, voltage, nailed):
-        super().__init__(ParrotType.EUROPEAN, number_of_coconuts, voltage, nailed)
+        super().__init__(ParrotType.AFRICAN, number_of_coconuts, voltage, nailed)
