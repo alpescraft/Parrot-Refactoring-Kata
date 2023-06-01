e79c335eee5ba7c4da36a8944b3eccd47fec334a
Vincent Huc <vincent.huc@pole-emploi.fr>
2023-06-01 11:56:09 +0200

    Correction classe norwegian

--- Kotlin/src/main/kotlin/parrot/NorwegianBlueParrot.kt
+++ Kotlin/src/main/kotlin/parrot/NorwegianBlueParrot.kt
@@ -1,1 +1,4 @@
-class NorwegianBlueParrot: Parrot ()
+package parrot
+
+class NorwegianBlueParrot(type: ParrotTypeEnum, numberOfCoconuts: Int, voltage: Double, isNailed: Boolean) :
+        Parrot(type, numberOfCoconuts, voltage, isNailed)
