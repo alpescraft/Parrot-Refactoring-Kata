370bc20ac206f2f24aa3c9489ebffa2c7e7c591a
DamienG <granierdamien@gmail.com>
2023-06-01 12:09:54 +0200

    suppression african constructeur avec type

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -1,9 +1,6 @@
 package parrot;
 
 public class AfricanParrot extends Parrot{
-    public AfricanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(type, numberOfCoconuts, voltage, isNailed);
-    }
 
     public AfricanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
         super(null, numberOfCoconuts, voltage, isNailed);
