55c6f23531f0572c1b00eabfef34cd7af3d2aa42
tibk <thibault.karrer@gmail.com>
2023-06-01 11:42:09 +0200

    Remove parrot type

--- Python/test_parrot.py
+++ Python/test_parrot.py
@@ -1,36 +1,36 @@
-from parrot import Parrot, ParrotType, AfricanParrot, EuropeanParrot, NorwegianParrot
+from parrot import Parrot, AfricanParrot, EuropeanParrot, NorwegianParrot
 
 
 def test_speedOfEuropeanParrot():
-    parrot = EuropeanParrot(ParrotType.EUROPEAN, 0, 0, False)
+    parrot = EuropeanParrot(0, 0, False)
     assert parrot.speed() == 12.0
 
 
 def test_speedOfAfricanParrot_With_One_Coconut():
-    parrot = AfricanParrot(ParrotType.AFRICAN, 1, 0, False)
+    parrot = AfricanParrot(1, 0, False)
     assert parrot.speed() == 3.0
 
 
 def test_speedOfAfricanParrot_With_Two_Coconuts():
-    parrot = AfricanParrot(ParrotType.AFRICAN, 2, 0, False)
+    parrot = AfricanParrot(2, 0, False)
     assert parrot.speed() == 0.0
 
 
 def test_speedOfAfricanParrot_With_No_Coconuts():
-    parrot = AfricanParrot(ParrotType.AFRICAN, 0, 0, False)
+    parrot = AfricanParrot(0, 0, False)
     assert parrot.speed() == 12.0
 
 
 def test_speedNorwegianBlueParrot_nailed():
-    parrot = NorwegianParrot(ParrotType.NORWEGIAN_BLUE, 0, 1.5, True)
+    parrot = NorwegianParrot(0, 1.5, True)
     assert parrot.speed() == 0.0
 
 
 def test_speedNorwegianBlueParrot_not_nailed():
-    parrot = NorwegianParrot(ParrotType.NORWEGIAN_BLUE, 0, 1.5, False)
+    parrot = NorwegianParrot(0, 1.5, False)
     assert parrot.speed() == 18.0
 
 
 def test_speedNorwegianBlueParrot_not_nailed_high_voltage():
-    parrot = NorwegianParrot(ParrotType.NORWEGIAN_BLUE, 0, 4, False)
+    parrot = NorwegianParrot(0, 4, False)
     assert parrot.speed() == 24.0
